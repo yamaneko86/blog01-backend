e94efa7ff79d88c0ccdd29b25a81ade70a355eb4
yoshiya0448 <yoshibasket86@gmail.com>
2023-04-11 15:30:04 +0900

    ✨ DRFでのブログのバックエンド開発

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Post(models.Model):
+  title = models.CharField('タイトル', max_length=50)
+  image = models.ImageField(upload_to='image', verbose_name='イメージ画像')
+  content = models.TextField('本文')
+  created_at = models.DateTimeField('作成日', auto_now_add=True)
+
+  def __str__(self) -> str:
+    return self.title
+
+    
---
+++ app/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from .serializers import PostSerializer
+from .models import Post
+
+class PostView(generics.ListAPIView):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializer
+
+class PostDetailView(generics.RetrieveAPIView):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializer
