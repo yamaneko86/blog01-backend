c724ce2c33b0fee971ef9dc6a250f4673307865b
yoshiya0448 <yoshibasket86@gmail.com>
2023-04-17 17:13:42 +0900

    ⚡️ DEBUG = True に修正

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('MY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'TRUE'
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
